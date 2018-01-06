e1be7d7dd5bec52da37063f75a26f56f64c21dea
robert renecker <sylvan.rrenecker10@gmail.com>
2018-01-05 16:12:37 -0800

    done

--- src/index.js
+++ src/index.js
@@ -57,14 +57,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
-
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length-1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[i]) {
@@ -77,18 +77,34 @@ class Game extends React.Component {
         squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
+
     });
   }
-  //create a constructor, which will create a state for the Game Component
-  //the only thing atm that the Game component needs to have within its state is
-  //the history of the game board, which it keeps track of.
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: ((step % 2) === 0),
+    });
+  }
 
 
 
   render(){
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+    });
     let status;
     //
     if (winner) {
@@ -110,7 +126,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
 
